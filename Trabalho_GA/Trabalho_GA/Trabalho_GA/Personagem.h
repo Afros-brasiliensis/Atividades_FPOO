0502c5374778c717a7e6ce9583788e51e99b2b28
Athos Nunes Kolling <athoskolling@gmail.com>
2025-09-19 19:44:55 -0300

    commit 2

--- Trabalho_GA/Trabalho_GA/Trabalho_GA/Personagem.h
+++ Trabalho_GA/Trabalho_GA/Trabalho_GA/Personagem.h
@@ -1,1 +1,45 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class Personagem {
+
+private: 
+	
+	//Atributos
+	int habilidade;
+	int energia;
+	int sorte;
+	
+
+public:
+
+	//Construtor
+	Personagem();
+	//Construtor com parametros
+	Personagem(int habilidade, int energia, int sorte);
+	//Destrutor
+	~Personagem();
+	
+	//Metodos para obtenção de atributos
+	int getHabilidade();
+	int getEnergia();
+	int getSorte();
+
+	//Metodos para modificar atributos
+	void setHabilidade(int habilidade);
+	void setEnergia(int energia);
+	void setSorte(int sorte);
+
+	//Metodos de combate e inventario
+	void testarSorte();
+	void atacar(Personagem& alvo);
+	void usarProvisao();
+	void adicionarItem(Item* item);
+
+
+};
+
